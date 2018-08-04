f567818691425cfe55029f157634a8c425f0a638
tranhongvien <hongvienqt@gmail.com>
2018-08-04 22:26:00 +0700

    text

---
+++ Untitled1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int a = 1;
+int number = 1;
+int  main ()  
+{
+	while (number < 100 )
+	{
+		number++;
+		a = a + number ;
+	}
+	cout << a << endl;	
+}
+
+
